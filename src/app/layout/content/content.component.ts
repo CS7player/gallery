8d219a1aedf8415901f84a1a3bee3f172671a491
CS7player <144120363+CS7player@users.noreply.github.com>
2024-11-28 21:24:18 +0530

    changes

--- src/app/layout/content/content.component.ts
+++ src/app/layout/content/content.component.ts
@@ -9,90 +9,97 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [FormsModule],
   templateUrl: './content.component.html',
-  styleUrl: './content.component.css'
+  styleUrl: './content.component.css',
 })
-export class ContentComponent implements OnInit{
-
-  imageData:any = [];
-  isResponse:boolean = false;
-  response:string = '';
-  imageSrc: string = "";
-  image_name:string = '';
-  picSrc:any=''
-  isZoom:boolean = false;
-  zoomPic:string = '';
-  constructor(private help : HelperService,private http : DataFetchService){}
+export class ContentComponent implements OnInit {
+  imageData: any = [];
+  isResponse: boolean = false;
+  response: string = '';
+  imageSrc: string = '';
+  image_name: string = '';
+  picSrc: any = '';
+  isZoom: boolean = false;
+  zoomPic: string = '';
+  constructor(private help: HelperService, private http: DataFetchService) {}
   ngOnInit(): void {
     this.getImages();
   }
   onChange(event: any) {
     this.imageSrc = event.target.files[0];
   }
-  addImage(){
+  addImage() {
+    if (this.imageSrc == '') {
+      this.alert('Select the Image!!!');
+      return;
+    }
+    if (this.image_name.length == 0) {
+      this.alert('Enter the Image Name!!!');
+      return;
+    }
     let details = this.help.getData('details');
     const formData = new FormData();
     formData.append('username', details.username);
     formData.append('id', details._id);
     formData.append('img', this.imageSrc);
-    formData.append('name',this.image_name);
-    this.http.doPost(ConstantsService.GALLERY_URL+'/pics/add', formData).subscribe({
+    formData.append('name', this.image_name);
+    this.http
+      .doPost(ConstantsService.GALLERY_URL + '/pics/add', formData)
+      .subscribe({
+        next: (res: any) => {
+          if (res['status']) {
+            this.alert(res['msg']);
+            // this.imageData.push({'username':details.username,'id': details._id,'img':this.picSrc,'name':this.imageData})
+          }
+        },
+        error: (err) => {
+          console.error('Error uploading image:', err);
+        },
+      });
+  }
+
+  getImages() {
+    this.alert('Fetching Images');
+    this.http.doGet(ConstantsService.GALLERY_URL + '/pics/details').subscribe({
       next: (res: any) => {
-        if(res['status']){
-          this.isResponse = true;
-          this.response = res['msg'];
-          this.getImages();
-          // this.imageData.push({'username':details.username,'id': details._id,'img':this.picSrc,'name':this.imageData})
+        if (res['status']) {
+          this.imageData = res['data'];
         }
       },
       error: (err) => {
-        console.error('Error uploading image:', err);
-      }
+        console.log(err);
+      },
     });
   }
 
-  getImages(){
-    this.isResponse = true;
-    this.response = 'fetching';
-    this.http.doGet(ConstantsService.GALLERY_URL+'/pics/details').subscribe({
-      next:(res:any)=>{
-        if(res['status']){
-          this.imageData = res['data']
-        }
-      },
-      error:(err)=>{
-        console.log(err)
-      }
-    })
-    this.clearResponse();
-  }
-
-  clearResponse(){
-    setTimeout(() => {
-    this.isResponse = false;
-    this.response = '';
-    },3000);
-  }
-
-  zoomImage(src : string){
-    this.zoomPic =  src;
+  zoomImage(src: string) {
+    this.zoomPic = src;
     this.isZoom = true;
   }
 
-  delImage(id : string){
-    this.http.doDelete(ConstantsService.GALLERY_URL+'/pics/delete?id='+id).subscribe({
-      next:(res:any)=>{
-        if(res['status']){
-          this.isResponse = true;
-          this.response = 'Image Deleted Successfully';
-          console.log(id);
-          this.imageData = this.imageData.filter((ele :any)=>{ele['_id'] != id});
-          console.log(this.imageData);
-        }
-      },
-      error:(err)=>{
-        console.log(err)
-      }
-    })
-    this.clearResponse();
+  delImage(id: string) {
+    this.http
+      .doDelete(ConstantsService.GALLERY_URL + '/pics/delete?id=' + id)
+      .subscribe({
+        next: (res: any) => {
+          if (res['status']) {
+            this.alert('Image Deleted Successfully!!!');
+            this.imageData = this.imageData.filter(
+              (ele: any) => ele['_id'] != id
+            );
+          }
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+  }
+
+  alert(alert: string) {
+    this.isResponse = true;
+    this.response = alert;
+    setTimeout(() => {
+      this.isResponse = false;
+      this.response = '';
+    }, 3000);
   }
 }
